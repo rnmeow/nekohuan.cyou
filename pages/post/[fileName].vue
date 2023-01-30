71916f79bb43c46ad0045051307b3bfaa4671cf1
郭桓桓 <hi@nekohuan.cyou>
2023-01-30 12:08:53 +0800

    fix: use `useLazyFetch()` method to avoid 500 error

--- pages/post/[fileName].vue
+++ pages/post/[fileName].vue
@@ -1,10 +1,11 @@
 <template>
   <article class="relative mx-auto max-w-5xl px-8 py-24 leading-8 font-sans">
     <Head>
-      <Title>{{ `${post.Title} | khh.log` }}</Title>
+      <Title v-if="pending">載入中…… | khh.log</Title>
+      <Title v-else>{{ `${post.Title} | khh.log` }}</Title>
     </Head>
-    <h1 class="font-bold dark:text-neutral-200" v-text="post.Title"></h1>
-    <div class="flex text-sm font-courier dark:text-neutral-300">
+    <h1 v-if="!pending" class="font-bold dark:text-neutral-200" v-text="post.Title"></h1>
+    <div v-if="!pending" class="flex text-sm font-courier dark:text-neutral-300">
       <p><FAIcon category="far" icon="calendar" size="md"/> {{ post.DateTime }}</p>
       &nbsp;/&nbsp;
       <p><FAIcon category="fas" icon="eye" size="sm"/> {{ post.Views.toString() }}</p>
@@ -14,7 +15,7 @@
         <div class="divide-y divide-gray-200 transition-colors dark:divide-neutral-700">
           <div class="pb-8 transition-colors lg:grid lg:grid-cols-4 lg:gap-x-6">
             <div class="divide-y divide-gray-200 pb-8 transition-colors dark:divide-neutral-700 lg:col-span-3">
-              <p v-if="pending">Loading...</p>
+              <p v-if="pending">載入中，請耐心等待……</p>
               <!-- eslint-disable vue/no-v-html -->
               <div
                 v-else
@@ -23,7 +24,7 @@
               ></div>
             </div>
             <aside>
-              <div class="hidden md:flex md:sticky md:top-24 md:col-span-1">
+              <div v-if="!pending" class="hidden md:flex md:sticky md:top-24 md:col-span-1">
                 <ToC element="#content"/>
               </div>
             </aside>
@@ -53,7 +54,7 @@ import Pangu from 'pangu'
 import { API_DOMAIN } from '@/config/links'
 
 const { fileName } = useRoute().params
-const { pending, data: post } = await useFetch<any>(() => `https://${API_DOMAIN}/post/${fileName}`)
+const { pending, data: post } = useLazyFetch<any>(() => `https://${API_DOMAIN}/post/${fileName}`, { server: false })
 const mdParser = new MarkdownIt({
   html: true,
   xhtmlOut: true,
