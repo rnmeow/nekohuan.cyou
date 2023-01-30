936ebfe034df23d3ca7ddacfbb211310a0ba9c22
郭桓桓 <hi@nekohuan.cyou>
2023-01-30 13:55:49 +0800

    chore: remove unused Tailwind rule

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -11,7 +11,6 @@ module.exports = {
     fontFamily: {
       'mono': ['DM Mono', 'Jetbrains Mono', 'Noto Sans TC', 'monospace'],
       'sans': ['Open Sans', 'Helvetica', 'Noto Sans TC', 'sans-serif'],
-      'serif': ['Times New Roman', 'Source Han Serif', 'Noto Serif TC', 'serif'],
       // Custom
       'courier': ['Courier Prime', 'Courier New', 'Courier', 'monospace']
     }
