a763983b4b885c22f8fdb3677dd0c12e5bfa31e1
郭桓桓 <hi@nekohuan.cyou>
2023-02-12 10:22:44 +0800

    refactor: split `Font Awesome` and `VueGTag`

---
+++ plugins/font-awesome.ts
@@ -0,0 +1,21 @@
+import { library, config } from '@fortawesome/fontawesome-svg-core'
+import {
+  faTag, faAngleLeft, faAngleRight, faAngleUp, faHeart, faRss, faBars, faEnvelope, faEye, faClockRotateLeft
+} from '@fortawesome/free-solid-svg-icons'
+import { faCopyright } from '@fortawesome/free-regular-svg-icons'
+import { 
+  faGolang, faVuejs, faGitAlt, faNodeJs, faGithub, faBitbucket, faWhatsapp, faTelegram, faStackOverflow
+} from '@fortawesome/free-brands-svg-icons'
+// eslint-disable-next-line import/named -- Temporary solution
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+config.autoAddCss = false
+library.add(
+  faTag, faAngleLeft, faAngleRight, faAngleUp, faHeart, faRss, faBars, faEnvelope, faEye,
+  faCopyright, faClockRotateLeft, faGolang, faVuejs, faGitAlt, faNodeJs,
+  faGithub, faBitbucket, faWhatsapp, faTelegram, faStackOverflow
+)
+
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.component('font-awesome-icon', FontAwesomeIcon)
+})
--- plugins/vuegtag.ts
+++ plugins/vuegtag.ts
@@ -1,24 +1,5 @@
-import { library, config } from '@fortawesome/fontawesome-svg-core'
-import {
-  faTag, faAngleLeft, faAngleRight, faAngleUp, faHeart, faRss, faBars, faEnvelope, faEye, faClockRotateLeft
-} from '@fortawesome/free-solid-svg-icons'
-import { faCopyright } from '@fortawesome/free-regular-svg-icons'
-import { 
-  faGolang, faVuejs, faGitAlt, faNodeJs, faGithub, faBitbucket, faWhatsapp, faTelegram, faStackOverflow
-} from '@fortawesome/free-brands-svg-icons'
-// eslint-disable-next-line import/named -- Temporary solution
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
 import VueGtag from 'vue-gtag-next'
 
-config.autoAddCss = false
-library.add(
-  faTag, faAngleLeft, faAngleRight, faAngleUp, faHeart, faRss, faBars, faEnvelope, faEye,
-  faCopyright, faClockRotateLeft, faGolang, faVuejs, faGitAlt, faNodeJs,
-  faGithub, faBitbucket, faWhatsapp, faTelegram, faStackOverflow
-)
-
 export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.component('font-awesome-icon', FontAwesomeIcon)
   nuxtApp.vueApp.use(VueGtag, { property: { id: 'G-3D83BY9RLC' } })
 })
